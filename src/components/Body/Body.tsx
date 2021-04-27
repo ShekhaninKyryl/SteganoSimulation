d87c64b5c747579b884db123c4a799739e6f4d52
Kyryl Shekhanin <kyryl.shekhanin@karazina.ua>
2021-04-27 17:26:19 +0300

    add Cluster component;

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,16 +1,31 @@
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import { FileSystem } from "../../entities/FileSystem/FileSystem";
+import Cluster from "../Cluster/Cluster";
+
+const useStles = makeStyles(() => ({
+  container: {
+    padding: 20,
+  },
+}));
 
 interface IBodyProps {
   fs?: FileSystem
 }
 
-
-const Body: React.FC<IBodyProps> = ({fs}) => {
+const Body: React.FC<IBodyProps> = ({ fs }) => {
   console.log(fs);
+
+  const classes = useStles();
   return (
     <div>
-      {fs?.clusters.map(c=><div>{c.fsIndex}</div>)}
+      <Grid container alignItems="flex-start" spacing={3} className={classes.container}>
+        {fs?.clusters.map(c => (
+          <Grid item xs={1}>
+            <Cluster cluster={c} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
---
+++ src/components/Cluster/Cluster.tsx
@@ -0,0 +1,54 @@
+import { Box, makeStyles } from "@material-ui/core";
+import React from "react";
+import clsx from 'clsx';
+import { Cluster as ClusterClass } from "../../entities/Claster/Claster";
+
+const useStles = makeStyles(() => ({
+  cluster: {
+    border: "1px solid",
+    borderRadius: 10,
+    padding: 8,
+    height: 64,
+  },
+
+  container: {
+    position: "relative",
+    height: "-webkit-fill-available",
+  },
+  fixed: {
+    position: "absolute",
+  },
+  index: {
+    right: 0,
+    bottom: 0,
+  },
+  fileName: {
+    left: 0,
+    top: 0,
+  },
+  fileIndex: {
+    right: 0,
+    top: 0,
+  }
+}));
+
+interface ICluster {
+  cluster: ClusterClass
+}
+
+const Cluster: React.FC<ICluster> = ({ cluster }) => {
+  const classes = useStles();
+
+  return (
+    <Box className={classes.cluster} style={{ borderColor: cluster.file?.color, backgroundColor: cluster.file?.color }}>
+      <div className={classes.container}>
+        <div className={clsx(classes.fixed, classes.index)}>{cluster.fsIndex}</div>
+        <div className={clsx(classes.fixed, classes.fileIndex)}>{cluster.fileIndex}</div>
+        <div className={clsx(classes.fixed, classes.fileName)}>{cluster.file?.name}</div>
+      </div>
+    </Box>
+  )
+}
+
+
+export default Cluster;

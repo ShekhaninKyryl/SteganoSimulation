0753f194a5ac7d2fa8d73f40ba0b97610ebbd3ed
Kyryl Shekhanin <kyryl.shekhanin@karazina.ua>
2021-08-26 23:04:08 +0300

    Add isBeforeReset

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+import { Box, Button, ButtonGroup, Typography, FormControlLabel, Checkbox } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -26,14 +26,38 @@ const Header: React.FC<IProps> = ({
   onReset }) => {
   const classes = useStyles();
 
+  const [isBeforeReset, setBeforeReset] = React.useState(false);
+
+  const handelDoReset = (method: () => void) => {
+    if (isBeforeReset) onReset();
+    method();
+  }
+
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={onBasic_I}>{"Basic-I"}</Button>
-      <Button variant="contained" color="primary" onClick={onBasic_II}>{"Basic-II"}</Button>
-      <Button variant="contained" color="primary" onClick={onBasic_III}>{"Basic-III"}</Button>
-      <Button variant="contained" color="primary" onClick={onBasic_IV}>{"Basic-IV"}</Button>
-      <Button variant="contained" onClick={onReset}>{"Reset"}</Button>
-    </div>
+    <Box display="flex" flexDirection="row" className={classes.root}>
+      <Box>
+        <Typography color="textPrimary" variant="h6">{"Basic methods"}</Typography>
+        <ButtonGroup variant="contained" color="primary" aria-label="text primary button group">
+          <Button onClick={() => handelDoReset(onBasic_I)}>{"Basic-I"}</Button>
+          <Button onClick={() => handelDoReset(onBasic_II)}>{"Basic-II"}</Button>
+          <Button onClick={() => handelDoReset(onBasic_III)}>{"Basic-III"}</Button>
+          <Button onClick={() => handelDoReset(onBasic_IV)}>{"Basic-IV"}</Button>
+        </ButtonGroup>
+      </Box>
+      <Box>
+        <Typography color="textPrimary" variant="h6">{"Improved methods"}</Typography>
+        <ButtonGroup disabled variant="contained" color="secondary" aria-label="text primary button group">
+          <Button onClick={() => handelDoReset(onBasic_I)}>{"Basic-I"}</Button>
+        </ButtonGroup>
+      </Box>
+      <Box flex={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
+        <FormControlLabel
+          control={<Checkbox checked={isBeforeReset} onChange={({ target: { checked } }) => setBeforeReset(checked)} name="isBeforeReset" color="default" />}
+          label="did reset before each going"
+        />
+        <Button variant="contained" onClick={onReset}>{"Reset"}</Button>
+      </Box>
+    </Box>
   )
 }
 
